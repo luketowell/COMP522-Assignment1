0974157580f6096611faac1f6def1dc18c06965e
Luke Towell <luke.towell@hotmail.com>
2019-11-02 20:47:22 +0000

    Added timings and loops into the code
    Added all timings into the code for each iteration and average timing for each password over set number of iterations.

--- code/PasswordBasedEncryption.java
+++ code/PasswordBasedEncryption.java
@@ -14,52 +14,86 @@ public class PasswordBasedEncryption {
         PBEParameterSpec pbeParamSpec;
         SecretKeyFactory keyFac;
 
-        // Salt
-        byte[] salt = {(byte) 0xc7, (byte) 0x73, (byte) 0x21,
-                (byte) 0x8c, (byte) 0x7e, (byte) 0xc8, (byte) 0xee, (byte) 0x99};
-        // Iteration count
-        int count = 1024;
+        // setup iteration count
+        int iterationCount = 5;
 
-        // Create PBE parameter set
-        pbeParamSpec = new PBEParameterSpec(salt, count);
+        // Setup passwords used to encrypt values
+        String[] passwords = new String[] { "abc", "P@ssW0rD", "Th!$IsAV3ryL0n9pA$$w0rd" };
 
-        //Initialization of the password
-        char[] password = "abc".toCharArray();
-        char newPassword = (char)(password[0]+1);
+        for (int i = 0; i < passwords.length; i++) {
 
-        System.out.println("password char array:" + newPassword);
+            // initialise time array
+            long[] time = new long[6];
 
+            for (int j = 0; j < iterationCount; j++) {
+                // start timing
+                long startTime = System.nanoTime();
+                // Salt
+                byte[] salt = { (byte) 0xc7, (byte) 0x73, (byte) 0x21, (byte) 0x8c, (byte) 0x7e, (byte) 0xc8,
+                        (byte) 0xee, (byte) 0x99 };
 
-        //Create parameter for key generation
-        pbeKeySpec = new PBEKeySpec(password);
+                // Iteration count
+                int count = 1024;
 
-        // Create instance of SecretKeyFactory for password-based encryption
-        // using DES and MD5
-        keyFac = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+                // Create PBE parameter set
+                pbeParamSpec = new PBEParameterSpec(salt, count);
 
-        // Generate a key
-        Key pbeKey = keyFac.generateSecret(pbeKeySpec);
+                // Initialization of the password
+                char[] password = passwords[i].toCharArray();
 
-        // Create PBE Cipher
-        Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
+                // Create parameter for key generation
+                pbeKeySpec = new PBEKeySpec(password);
 
-        // Initialize PBE Cipher with key and parameters
-        pbeCipher.init(Cipher.ENCRYPT_MODE, pbeKey, pbeParamSpec);
-        // Our plaintext
+                // Create instance of SecretKeyFactory for password-based encryption
+                // using DES and MD5
+                keyFac = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
 
-        byte[] cleartext = "This is another example".getBytes();
+                // Generate a key
+                Key pbeKey = keyFac.generateSecret(pbeKeySpec);
 
-        // Encrypt the plaintext
+                // Create PBE Cipher
+                Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
 
-        byte[] ciphertext = pbeCipher.doFinal(cleartext);
-        System.out.println("cipher : " + Utils.toHex(ciphertext));
+                // Initialize PBE Cipher with key and parameters
+                pbeCipher.init(Cipher.ENCRYPT_MODE, pbeKey, pbeParamSpec);
 
-        pbeCipher.init(Cipher.DECRYPT_MODE, pbeKey, pbeParamSpec);
+                // Our plaintext
+                byte[] cleartext = "This is another example".getBytes();
 
-        // Decrypt the plaintext
-        byte[] ciphertext2 = pbeCipher.doFinal(ciphertext);
-        String plainText = new String(ciphertext2);
+                // Encrypt the plaintext
+                byte[] ciphertext = pbeCipher.doFinal(cleartext);
 
-        System.out.println("password: " + plainText);
+                pbeCipher.init(Cipher.DECRYPT_MODE, pbeKey, pbeParamSpec);
+
+                // Decrypt the plaintext
+                byte[] ciphertext2 = pbeCipher.doFinal(ciphertext);
+                String plainText = new String(ciphertext2);
+
+                // end time
+                long endTime = System.nanoTime();
+
+                // calculate total time and add to the time array
+                long totalTime = endTime - startTime;
+                long totalTimeMs = totalTime / 1000000;
+                time[j] = totalTimeMs;
+
+                // output of all times and key components of the encryption algorithm
+                System.out.println("password used: " + passwords[i]);
+                System.out.println("cipher : " + Utils.toHex(ciphertext));
+                System.out.println("Encrypted value: " + plainText);
+                System.out
+                        .println("time taken(ms) to run encryption and decryption for loop " + j + ": " + totalTimeMs);
+            }
+            // summed time calculation to output average over the iteration counts.
+            long summedTime = 0;
+            for (var k = 0; k < time.length; k++) {
+                summedTime += time[k];
+            }
+
+            // Work out and print the average time for each password
+            System.out.println("Total time to run 5 iterations: " + summedTime);
+            long avgTime = summedTime / iterationCount;
+            System.out.println("average time take for " + iterationCount + " iterations: " + avgTime);
+        }
     }
 }
